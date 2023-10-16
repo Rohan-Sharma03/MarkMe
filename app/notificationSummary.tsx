27faa173d3409c41d69d3b60a7758d9d2e2e96fd
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-16 17:35:45 +0530

    Updated Component and file name

---
+++ app/notificationSummary.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function DetailNotification(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is detailed notification</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
