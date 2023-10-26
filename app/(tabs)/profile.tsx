42aa6236dded6373b63119676ae6e6d31b1ce8bd
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-26 21:52:03 +0530

    No major changes were found.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -9,7 +9,7 @@ export const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function TabTwoScreen() {
-  const router = useRouter();
+    const router = useRouter();
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
