a18b79eece821fafadc37c82b760e660d4b43c98
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-26 21:55:52 +0530

    The files is updated with the varibales required to handle the data passed in the component from home.tsx, it is used to display data on the screen

--- app/courseDetail.tsx
+++ app/courseDetail.tsx
@@ -1,12 +1,19 @@
 import { View, Text } from "react-native";
 import CourseHeader from "../components/CourseBox/CourseHeader";
 import CourseBody from "../components/CourseBox/CourseBody";
-
+import { useRouter } from "expo-router";
+import { useRoute } from "@react-navigation/native";
+type RouteParams = {
+  course_name: string;
+  // Add other expected parameters here
+};
 export default function CourseDetail(): JSX.Element {
+  const route = useRoute();
+  const { course_name } = route.params as RouteParams;
   return (
     <View style={{ backgroundColor: "white" }}>
-      {/* <Text>Course Details</Text> */}
-      <CourseHeader />
+      <Text>{course_name}</Text>
+      <CourseHeader course_name={course_name} />
       <CourseBody />
     </View>
   );
