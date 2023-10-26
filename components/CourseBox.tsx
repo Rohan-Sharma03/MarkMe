de9b0bfc070ded4cc3af34ef326d294128f711d2
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-26 22:26:18 +0530

    Tried working with various methods to pass data using naviagte, still figuring out on this

--- components/CourseBox.tsx
+++ components/CourseBox.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { Image } from "expo-image";
-import { Link } from "expo-router";
+
+import { useNavigation } from "@react-navigation/native";
+import { Link, useRouter } from "expo-router";
 import { View, Text, Pressable, StyleSheet } from "react-native";
 export const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
-export default function CourseBox(): JSX.Element {
+export default function CourseBox({
+  course_name,
+  course_id,
+}: {
+  course_name: string;
+  course_id: string;
+}): JSX.Element {
+  const facultyInOffice = false; // Change this to determine if the faculty is in the office
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const handlePressC = () => {
+    router.push({ pathname: "/courseDetail" });
+  };
+  const handlePressQR = () => {
+    router.push("/qrScan");
+  };
+
   return (
     <View style={styles.elevation}>
       <View style={styles.card}>
@@ -17,23 +36,27 @@ export default function CourseBox(): JSX.Element {
             backgroundColor: "#DAF7A6",
           }}
         ></View>
-        <Link href="/qrScan" asChild>
-          <Pressable>
-            {({ pressed }) => (
-              <View style={{ opacity: pressed ? 0.5 : 1 }}>
-                <FontAwesome
-                  style={{ left: 130, top: -70 }}
-                  name="qrcode"
-                  size={40}
-                  color="#60a5fa"
-                />
-              </View>
-            )}
-          </Pressable>
-        </Link>
+        <Pressable onPress={handlePressQR}>
+          {({ pressed }) => (
+            <View style={{ opacity: pressed ? 0.5 : 1 }}>
+              <FontAwesome
+                style={{ left: 130, top: -70 }}
+                name="qrcode"
+                size={40}
+                color="#60a5fa"
+              />
+            </View>
+          )}
+        </Pressable>
 
-        <Link href="/courseDetail" asChild>
-          <Pressable>
+        <Link
+          href={{
+            pathname: "/courseDetail",
+            params: { id: "rohan" },
+          }}
+          asChild
+        >
+          <Pressable onPress={handlePressC}>
             {({ pressed }) => (
               <View style={{ opacity: pressed ? 0.5 : 1 }}>
                 <Text
@@ -42,9 +65,12 @@ export default function CourseBox(): JSX.Element {
                     fontSize: 20,
                     fontWeight: "bold",
                     paddingLeft: 10,
+                    width: 180,
                   }}
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
                 >
-                  NoSQl
+                  {course_name}
                 </Text>
               </View>
             )}
@@ -58,8 +84,15 @@ export default function CourseBox(): JSX.Element {
             paddingLeft: 10,
           }}
         >
-          CS116
+          {course_id}
         </Text>
+        <View style={{ position: "absolute", top: 120, right: 20 }}>
+          <MaterialIcons
+            name={facultyInOffice ? "schedule" : "schedule-send"}
+            size={30}
+            color={facultyInOffice ? "green" : "red"}
+          />
+        </View>
       </View>
     </View>
   );
