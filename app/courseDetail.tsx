7f566210f399f68ab524625ce06891123982d2a3
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-27 22:14:48 +0530

    The data was properly extracted from the params

--- app/courseDetail.tsx
+++ app/courseDetail.tsx
@@ -1,19 +1,26 @@
 import { View, Text } from "react-native";
 import CourseHeader from "../components/CourseBox/CourseHeader";
 import CourseBody from "../components/CourseBox/CourseBody";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
 import { useRoute } from "@react-navigation/native";
 type RouteParams = {
   course_name: string;
   // Add other expected parameters here
 };
 export default function CourseDetail(): JSX.Element {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const params = useLocalSearchParams();
+  const { id, course_name, course_objective } = params;
+
+  console.log(id, course_name);
   const route = useRoute();
-  const { course_name } = route.params as RouteParams;
   return (
     <View style={{ backgroundColor: "white" }}>
-      <Text>{course_name}</Text>
-      <CourseHeader course_name={course_name} />
+      <CourseHeader
+        course_name={course_name.toString()}
+        course_objective={course_objective.toString()}
+      />
       <CourseBody />
     </View>
   );
