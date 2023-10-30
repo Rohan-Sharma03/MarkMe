f723928bdde4f7b3774f6754872fd8647955a425
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-30 23:06:55 +0530

    Update the Header section

--- components/CourseBox/CourseHeader.tsx
+++ components/CourseBox/CourseHeader.tsx
@@ -1,43 +1,46 @@
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-export default function CourseHeader({
+
+const CourseHeader = ({
   course_name,
   course_objective,
 }: {
   course_name: string;
   course_objective: string;
-}): JSX.Element {
+}): JSX.Element => {
   return (
-    <View style={Styles.container}>
-      <Text style={Styles.title}>{course_name}</Text>
-      <Text style={Styles.subtitle}> {course_objective}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{course_name}</Text>
+      <Text style={styles.subtitle}>{course_objective}</Text>
     </View>
   );
-}
-const Styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    padding: 10,
+    padding: 20,
     height: 200,
-    backgroundColor: "#D5E7F7",
+    backgroundColor: "#6C63FF",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   title: {
-    fontSize: 35,
+    fontSize: 32,
     fontWeight: "bold",
     marginBottom: 10,
-    color: "#333", // Text color (you can change the value)
-    lineHeight: 40, // Line height for the title (you can change the value)
+    color: "#FFFFFF",
+    lineHeight: 40,
+    textAlign: "center",
   },
   subtitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10,
-    color: "#666", // Text color (you can change the value)
-    lineHeight: 24, // Line height for the subtitle (you can change the value)
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    fontSize: 18,
+    fontWeight: "normal",
+    color: "#FFFFFF",
+    lineHeight: 24,
+    textAlign: "center",
   },
 });
+
+export default CourseHeader;
