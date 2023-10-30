ead9c996cb6bfa431d65ec4ddbc3c66a0d109abe
Rohan-Sharma03 <therohansharma03@gmail.com>
2023-10-30 23:02:40 +0530

    Course Id was passed to Time Table

--- app/schedule.tsx
+++ app/schedule.tsx
@@ -1,26 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import TimeTable from "../components/timetable";
 import { ScrollView } from "react-native-gesture-handler";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
 export default function schedule(): JSX.Element {
+  const router = useRouter();
+  const params = useLocalSearchParams();
+  const { id, course_name, course_id, course_objective } = params;
+  // console.log("schedelue", course_id);
+
+  console.log("The course ID is :", course_id);
+
   return (
     <ScrollView>
       <View style={styles.container}>
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
-        <TimeTable name="rohan" />
+        <TimeTable
+          course_id={typeof course_id === "string" ? course_id : course_id[0]}
+        />
       </View>
     </ScrollView>
   );
